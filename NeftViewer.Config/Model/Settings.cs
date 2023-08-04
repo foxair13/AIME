0a5da315e429cc0966e7e9331a689b5f405c5901
root <admin@example.com>
2023-08-04 14:26:05 +0300

    rr

---
+++ NeftViewer.Config/Model/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeftViewer.Config.Model
+{
+    public class Settings
+    {
+        public string ApiKey { get; set; }
+        public bool IsEnabled { get; set; }
+        public int MaxItemCount { get; set; }
+    }
+}
