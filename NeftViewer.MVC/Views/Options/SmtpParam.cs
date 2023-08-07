7da9d5df14fcb7a70470293e86b8b60a3d01e7a8
root <admin@example.com>
2023-08-07 08:10:42 +0300

    Smtp Options

---
+++ NeftViewer.MVC/Views/Options/SmtpParam.cs
@@ -0,0 +1,12 @@
+﻿namespace NeftViewer.MVC.Views.Options
+{
+    public class SmtpParam
+    {
+        public string Server { get; set; }
+        public string Port { get; set; }
+        public string Sender { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
