5b2a247f868527a01a3ef32765904e241dfc85c9
root <admin@example.com>
2023-10-20 09:52:18 +0300

    Изменения по календарю

---
+++ NeftViewer.MVC/Models/SeriesData.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace NeftViewer.MVC.Models
+{
+    public class SeriesData
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Stack { get; set; }
+        public List<int> Data { get; set; }
+
+        public SeriesData(string name, string type, string stack, List<int> data)
+        {
+            Name = name;
+            Type = type;
+            Stack = stack;
+            Data = data;
+        }
+
+        public string ToString()
+        {
+            return JsonConvert.SerializeObject(new { name = Name, type = Type, stack = Stack, data = Data });
+        }
+    }
+}
